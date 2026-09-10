d6176dcc39b27c9411b3a227eee88632e9dddfbc
vddCore <vddcore@outlook.com>
2026-09-10 16:29:43 +0200

    add tab strip and tab item templates.

--- Tempest.UI.Example/MainWindow.axaml.cs
+++ Tempest.UI.Example/MainWindow.axaml.cs
@@ -1,11 +1,43 @@
 namespace Tempest.UI.Example;
 
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
 using Tempest.UI.Controls;
 
 public partial class MainWindow : PlasmaWindow
 {
+    public static readonly StyledProperty<Dock> CurrentStripPlacementProperty 
+        = AvaloniaProperty.Register<MainWindow, Dock>(nameof(CurrentStripPlacement));
+
+    public Dock CurrentStripPlacement
+    {
+        get => GetValue(CurrentStripPlacementProperty);
+        set => SetValue(CurrentStripPlacementProperty, value);
+    }
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void SetTabStripPlacement(object? placement)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            var oldValue = CurrentStripPlacement;
+            CurrentStripPlacement = (Dock)placement;
+            
+            OnPropertyChanged(new AvaloniaPropertyChangedEventArgs<Dock>(
+                this, 
+                CurrentStripPlacementProperty,
+                oldValue,
+                CurrentStripPlacement,
+                BindingPriority.LocalValue
+            ));
+        });
+
+
+
     }
 }
